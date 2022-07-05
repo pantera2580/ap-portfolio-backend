dc73501b9e9e22a51930ea94bd70df2d581bb645
pantera2580 <emmnlcruz@gmail.com>
2022-07-05 18:20:31 -0300

    Initial commit

---
+++ src/main/java/com/mec/apportfoliobackend/ApPortfolioBackendApplication.java
@@ -0,0 +1,13 @@
+package com.mec.apportfoliobackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApPortfolioBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApPortfolioBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mec/apportfoliobackend/ApPortfolioBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mec.apportfoliobackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApPortfolioBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
